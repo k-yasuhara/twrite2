27557aa0ab9fcae7ad106af309bd0f13d858fff5
zd3N14 <emai.domain@com>
2025-03-11 11:13:09 +0900

    ログインフィルターの修正

--- src/main/java/com/example/app/filter/AuthFilter.java
+++ src/main/java/com/example/app/filter/AuthFilter.java
@@ -2,6 +2,7 @@ package com.example.app.filter;
 
 import java.io.IOException;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import jakarta.servlet.FilterChain;
@@ -9,6 +10,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+@Configuration
 public class AuthFilter extends OncePerRequestFilter {
 
 	@Override
@@ -16,7 +18,14 @@ public class AuthFilter extends OncePerRequestFilter {
 			HttpServletRequest request, HttpServletResponse response,
 			FilterChain filterChain)
 			throws ServletException, IOException {
-
+		
+		// ログイン画面や静的リソースには適用しない
+        String uri = request.getRequestURI();
+        if (uri.equals("/") ||uri.equals("/login") || uri.equals("/logout") || uri.startsWith("/css/") || uri.startsWith("/js/") || uri.startsWith("/images/")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+ 		
 		if (request.getSession().getAttribute("loginId") == null) {
 			response.sendRedirect("/login");
 			return;
